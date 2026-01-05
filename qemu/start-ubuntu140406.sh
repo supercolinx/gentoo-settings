f82e1f9286e106426852b75a27346d237cc952a9
supercolinx <xiexienio@foxmail.com>
2026-01-05 20:01:16 +0800

    update some configs

--- qemu/start-ubuntu140406.sh
+++ qemu/start-ubuntu140406.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-TARGET=ubuntu2404.qcow2
+TARGET=ubuntu140406.qcow2
 
 if [ ! -f ${TARGET} ]
 then
@@ -7,18 +7,18 @@ then
 	qemu-img create -f qcow2 ${TARGET} 32G
 fi
 
-	#-audiodev alsa,id=audio0,out.dev=default,in.dev=default \
-	#-device ac97,audiodev=audio0 \
-	#-device virtio-keyboard-pci \
-	#-device virtio-tablet-pci \
 qemu-system-x86_64 \
 	-machine type=q35,accel=kvm \
         -cpu host \
         -m 4G \
-	-vga virtio \
 	-smp cpus=4,cores=4 \
+	-vga cirrus \
 	-drive file=${TARGET},if=virtio \
 	-display sdl,gl=on,grab-mod=rctrl \
+	-device virtio-keyboard-pci \
+	-usb -device usb-tablet \
+	-audiodev alsa,id=audio0,out.dev=default,in.dev=default \
+	-device ac97,audiodev=audio0 \
 	-nic user,hostfwd=tcp::2222-:22 \
         $@ &
 
